1d3145ba00bd7a51317974bfe4fefa11bf369de6
tostitos <camila.orellana@boosmap.com>
2016-07-06 01:35:23 -0400

    date.now

--- models/evento.js
+++ models/evento.js
@@ -7,9 +7,10 @@ var options = {
 mongoose.connect('mongodb://'+process.env.OPENSHIFT_MONGODB_DB_HOST+':'+process.env.OPENSHIFT_MONGODB_DB_PORT+'/'+'halconservice', options);
 var Schema = mongoose.Schema;
 
+
 var eventoSchema = new Schema({
-	fecha: 			{ type: Date, default: Date.now }, 
-	categoria: 		{ type: String, enum: ['info','error','warning']	} /* info, error, warning*/,
+	//fecha: 			{ type: Date, default: Date.now }, 
+	categoria: 		{ type: String, enum: ['info','error','warning']	}, /* info, error, warning*/
 	descripcion: 	{ type: String 	}
 });
 
